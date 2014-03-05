17f4d28990c5beeb6b83826072f8270e3599fa2c
Olivier Lalonde <olalonde@gmail.com>
2014-03-05 13:22:31 +0800

    added some parsing tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
 var assert = require('assert'),
   fs = require('fs'),
   bitscript = require('../'),
-  compile = bitscript.compile;
+  compile = bitscript.compile,
+  nodes = bitscript.nodes,
+  parse = bitscript.parse;
 
 var DATA_PATH = __dirname + '/data/';
 
@@ -14,15 +16,50 @@ fs.readdirSync(DATA_PATH).forEach(function (file) {
 
 //@todo test parsing
 
+describe('Parsing', function () {
+  files.forEach(function (file) {
+    var source = fs.readFileSync(DATA_PATH + file + '.bitscript', 'utf8');
+
+    describe(file + '.bitscript', function () {
+      var ast;
+
+      it('should not throw errors', function () {
+        ast = parse(source);
+      });
+
+      it('should not return an object', function () {
+        assert.equal('object', typeof ast);
+      });
+
+      it('should return a Root node', function () {
+        assert.ok(ast instanceof nodes.Root);
+      });
+
+      it('the Root node should be an instance of Base node', function () {
+        assert.ok(ast instanceof nodes.Base);
+      });
+
+    });
+
+  });
+});
+
 describe('Compilation', function () {
   files.forEach(function (file) {
     var source = fs.readFileSync(DATA_PATH + file + '.bitscript', 'utf8');
     var expected = fs.readFileSync(DATA_PATH + file + '.res', 'utf8');
-    describe(file + '.bitscript compilation', function () {
-      var result = compile(source);
+
+    describe(file + '.bitscript', function () {
+      var result;
+
+      it('should not throw errors', function () {
+        result = compile(source);
+      });
+
       it('should produce expected code', function () {
         assert.equal(expected, result);
       });
+
     });
   });
 });
